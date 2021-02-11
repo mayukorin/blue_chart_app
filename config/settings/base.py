d10324db39ea8a2299f511af7e121f208cec623b
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-02-11 19:16:46 +0900

    Modify base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,10 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = {
-    os.path.join(BASE_DIR,"static"),
-    }
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #LOGIN_URL='siteUser:login'
