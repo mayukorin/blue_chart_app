dfb14a4782b3db1e475f2faded01c014d6c775ff
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-02-13 12:47:11 +0900

    Add github

--- blue_chart_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlueChartAppConfig(AppConfig):
-    name = 'blue_chart_app'
--- blue_chart_app/models/subject.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Subject(models.Model):
-
-    name = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
